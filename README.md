062c2c6f83aaaa78c32aeccdbe365e50860d697c
Rob Calcroft <rob@calcroft.com>
2014-03-18 19:26:32 +0000

    Improved design and added validation to entries

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 diary
 =====
 
-A JSON based diary system
+###### A JSON based diary system
+
+This app uses **JavaScript** and **PHP** to write and get the contents of a **JSON** file which holds the list of user's diary entries.
+
+It has an *add* and an *edit* page where the user can modify the content.
--- diary.js
+++ diary.js
@@ -1,16 +1,73 @@
-	
-	$.getJSON( "entries.json", function( data ) {
-		var entries = data;
-	  	$('.entries-body').html(function() {
-	  		var i = 1, entry;
-	  		while(entry = entries[i]) {
-	  			return makeEntry(entry);
-	  			i++
-	  		}
-	  })
-	 });
+	$(document).ready(function() {
+		$('#add-entry').bind('submit', newEntry);
+		pageHighlight();
+		$('#add-entry textarea').click(function() {
+			$('.error').hide('quick');
+			$('#add-entry textarea').removeClass('box-error');
+
+		})
+	})
+
+
+	function getJSON() {		
+		$.getJSON( "entries.json", function( entries ) {
+				var i = countObject(entries), entry;
+			  	while(entry = entries[i]) {
+			  		$('.entries-body').append(function() {
+			  			i--;
+			  			return makeEntry(entry);
+			  		})		
+			  	}
+
+		})
+		.fail(function(){
+			var error = "No entries were found, <a href='/diary/new/' class='underline'>click here</a> to add one"
+			$('.entries-body').html(error);
+		});
+	}
+
+	function newEntry(event) {
+		event.preventDefault();
+		var entry = document.querySelector('#add-entry textarea').value;
+		if(entry == "") {
+			$('.error').show('quick');
+			$('#add-entry textarea').addClass('box-error');
+			return false;
+		} else { 
+			$('#success').show('quick');
+			$('#add-entry textarea').addClass('box-success') 
+		}
+		$.ajax({
+			type : "POST",
+			url : 'writeToJSON.php',
+			data : { data : entry }
+		})
+		.done(function(data_d) {
+			setTimeout(function() {
+				$('#success').hide('quick');
+				$('#add-entry textarea').removeClass('box-success');
+			},1500);
+		})
+	}
 
 	function makeEntry (entry) {
-		var article = '<article>' + entry.date + '<section>' + entry.content + '</section></article>';
+		var article = '<article id=' + entry.date + '><span class="underline">' + entry.date + '</span><section>' + entry.content + '</section></article>';
 		return article;
+	}
+
+	function countObject( object ) {
+	    var length = 0;
+	    for( var key in object ) {
+	        if( object.hasOwnProperty(key) ) {
+	            ++length;
+	        }
+	    }
+	    return length;
+	};
+
+	function pageHighlight () {
+		var url =  document.URL;
+    	if(url.indexOf('new') != -1) { document.querySelector('.new-nav').classList.add('nav-highlight') }
+   	 	else if(url.indexOf('edit') != -1) { document.querySelector('.edit-nav').classList.add('nav-highlight') }
+   	 	else { document.querySelector('.entries-nav').classList.add('nav-highlight') }
 	}
